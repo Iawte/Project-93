acd3174fc33d3be30bc058dea64512a737fe9cc1
Iawte <79056849+Iawte@users.noreply.github.com>
2021-07-02 13:38:21 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+var player1Name,player2Name;
+
+function addUser(){
+    player1Name=document.getElementById("player1Name").value;
+    player2Name=document.getElementById("player2Name").value;
+localStorage.setItem("player1Name",player1Name);
+localStorage.setItem("player2Name",player2Name);
+}
+
+window.location = "mathpage.html";
